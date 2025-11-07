f7a617fb7503f94768a3960bda48f211de35670f
HaritsTama <nayotama1818@gmail.com>
2025-11-07 18:29:17 +0700

    Menambahkan state textNamaLengkap pada function FormulirP untuk menyimpan input nama lengkap

--- app/src/main/java/com/example/navigasipraktikum/view/Formulir.kt
+++ app/src/main/java/com/example/navigasipraktikum/view/Formulir.kt
@@ -1,12 +1,19 @@
 package com.example.navigasipraktikum.view
 
 
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+
 
 
 @Composable
 fun FormulirP(modifier: Modifier
 ) {
+    var textNamaLengkap by remember { mutableStateOf("") }
 
 }
