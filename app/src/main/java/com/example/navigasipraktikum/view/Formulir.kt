7d471feffa0cdb931b331ee4510a0f12779bb4ff
HaritsTama <nayotama1818@gmail.com>
2025-11-07 18:25:37 +0700

    Membuat package baru, Formulir.kt dan function FormulirP

---
+++ app/src/main/java/com/example/navigasipraktikum/view/Formulir.kt
@@ -0,0 +1,12 @@
+package com.example.navigasipraktikum.view
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FormulirP(modifier: Modifier
+) {
+
+}
